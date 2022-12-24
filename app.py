c3e812a43ba10c3c88f3242f835b85b98f32d84c
HamedSDZ <hsadeghzadeh79@gmail.com>
2022-12-25 02:15:57 +0330

    initial project

---
+++ app.py
@@ -0,0 +1,24 @@
+import tensorflow_hub as hub
+import tensorflow as tf
+from matplotlib import pyplot as plt
+import numpy as np
+import cv2
+
+model = hub.load('mg')
+
+
+def load_image(img_path):
+    img = tf.io.read_file(img_path)
+    img = tf.image.decode_image(img, channels=3)
+    img = tf.image.convert_image_dtype(img, tf.float32)
+    img = img[tf.newaxis, :]
+    return img
+
+
+content_image = load_image('porsche.jpeg')
+style_image = load_image('bnz.jpg')
+
+stylized_image = model(tf.constant(content_image), tf.constant(style_image))[0]
+
+cv2.imwrite('generated_img.jpg', cv2.cvtColor(
+    np.squeeze(stylized_image)*255, cv2.COLOR_BGR2RGB))
